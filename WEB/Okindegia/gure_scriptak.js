d51936284855aa6ab8f4f63e28f01adb55ee4a97
plopezarco <lopez.pablo@uni.eus>
2021-09-30 09:00:41 +0200

    login

--- WEB/Okindegia/gure_scriptak.js
+++ WEB/Okindegia/gure_scriptak.js
@@ -18,7 +18,7 @@ function pasahitzaKonprobatu() {
     document.getElementById("wrong").style.display = "block";
 }
 
-function pasahitzaKonprobatu2() {
+function login() {
     var erabiltzaileak = [
         { user: "Pablo", pass: "1234" },
         { user: "Admin", pass: "Admin123" },
@@ -29,14 +29,29 @@ function pasahitzaKonprobatu2() {
 
     erabiltzaileak.forEach((element) => {
         if (element.user == user && element.pass == pass) {
-            window.open(
-                "file:///C:/Users/lopez.pablo/Documents/GitHub/teampop/JS/login.html",
-                "_self"
-            );
+            sessionStorage.Izena = element.user;
+            sessionStorage.Loged = true;
+            alert("Ongi etorri, " + sessionStorage.Izena);
+            document.getElementById("login1").style.display = "none";
+            document.getElementById("login2").style.display = "none";
+            document.getElementById("logout1").style.display = "block";
+            document.getElementById("logout2").style.display = "block";
             return;
         }
     });
-    document.getElementById("wrong").style.display = "block";
+
+    if (!sessionStorage.Loged) {
+        alert("Saiatu berriz");
+    }
+}
+
+function logout() {
+    sessionStorage.Izena = null;
+    sessionStorage.Loged = false;
+    document.getElementById("login1").style.display = "block";
+    document.getElementById("login2").style.display = "block";
+    document.getElementById("logout1").style.display = "none";
+    document.getElementById("logout2").style.display = "none";
 }
 
 function data() {
@@ -50,33 +65,33 @@ function data() {
 }
 
 function erosketa() {
-    var r = /^\+?(0|[1-9]\d*)$/
-    var bezero_izena = prompt("Sartu zure izena:")
-    var saski_num
+    var r = /^\+?(0|[1-9]\d*)$/;
+    var bezero_izena = prompt("Sartu zure izena:");
+    var saski_num;
     do {
-        saski_num = prompt("Sartu erosiko dituzun produktu kopurua:")
-    } while (!r.test(saski_num))
+        saski_num = prompt("Sartu erosiko dituzun produktu kopurua:");
+    } while (!r.test(saski_num));
 
-    var produktuak = []
+    var produktuak = [];
     for (var i = 0; i < saski_num; i++) {
-        var izena = prompt("Sartu " + (parseInt(i) + 1) + ". produktua: ")
+        var izena = prompt("Sartu " + (parseInt(i) + 1) + ". produktua: ");
         var kopurua, prezioa;
         do {
-            kopurua = prompt("Sartu " + izena + " kopurua: ")
-        } while (!r.test(kopurua))
+            kopurua = prompt("Sartu " + izena + " kopurua: ");
+        } while (!r.test(kopurua));
         do {
-            prezioa = prompt("Sartu " + izena + " prezioa: ")
-        } while (!r.test(prezioa))
-        produktuak.push({ izena: izena, kopurua: kopurua, prezioa: prezioa })
+            prezioa = prompt("Sartu " + izena + " prezioa: ");
+        } while (!r.test(prezioa));
+        produktuak.push({ izena: izena, kopurua: kopurua, prezioa: prezioa });
     }
 
-    var mezua = bezero_izena + ", hau da zure erosketa saskia: \n         "
+    var mezua = bezero_izena + ", hau da zure erosketa saskia: \n         ";
     produktuak.forEach((element) => {
-        mezua += element.izena + ", Unitateak: " + element.kopurua + "\n         "
-    })
-    alert(mezua)
+        mezua += element.izena + ", Unitateak: " + element.kopurua + "\n         ";
+    });
+    alert(mezua);
 
-    kalkulatuPrezioa(produktuak)
+    kalkulatuPrezioa(produktuak);
 }
 
 function bezeroaGorde() {
@@ -109,7 +124,9 @@ function kalkulatuPrezioa(produktuak) {
 var mybutton = document.getElementById("scroll_top");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() { scrollFunction() };
+window.onscroll = function() {
+    scrollFunction();
+};
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -117,4 +134,16 @@ function scrollFunction() {
     } else {
         mybutton.style.display = "none";
     }
+}
+
+if (sessionStorage.Loged == "true") {
+    document.getElementById("login1").style.display = "none";
+    document.getElementById("login2").style.display = "none";
+    document.getElementById("logout1").style.display = "block";
+    document.getElementById("logout2").style.display = "block";
+} else {
+    document.getElementById("login1").style.display = "block";
+    document.getElementById("login2").style.display = "block";
+    document.getElementById("logout1").style.display = "none";
+    document.getElementById("logout2").style.display = "none";
 }
