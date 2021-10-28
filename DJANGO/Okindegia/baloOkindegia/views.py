ec74426a0457b83ce5babc022bf9ade5b6dcef5c
plopezarco <lopez.pablo@uni.eus>
2021-10-28 08:47:46 +0200

    produktuak lista

--- DJANGO/Okindegia/baloOkindegia/views.py
+++ DJANGO/Okindegia/baloOkindegia/views.py
@@ -8,7 +8,7 @@ def produktuak(request):
     produktuak_list = Produktua.objects.all()
     dicc = {'produktuak' : produktuak_list}
     
-    return render(request, "produktuak2.html", dicc)
+    return render(request, "produktuak.html", dicc)
 
 def index(request):
     return render(request, "index.html")
